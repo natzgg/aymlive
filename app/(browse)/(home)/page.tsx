9f1c616bbcf8e78887f022b71d742b0002a3e23d
natzgg <natzcunanan@gmail.com>
2024-04-17 16:17:29 +0900

    Remove type error

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -6,7 +6,6 @@ export default function Page() {
   return (
     <div className="h-full p-8 max-w-screen-2xl mx-auto">
       <Suspense fallback={<ResultsSkeleton />}>
-        {/* @ts-expect-error Server Component */}
         <Results />
       </Suspense>
     </div>
